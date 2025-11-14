b04a9e9b0dd96b45a02c58132cf3ce90eafadfb9
Victor Lacomb de Melo <57971012+lacomb04@users.noreply.github.com>
2025-11-14 20:12:33 +0000

    foto integrantes grupo

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -67,7 +67,7 @@ export default function AboutSection() {
             <div className="mb-6">
               <div className="w-full max-w-2xl h-48 md:h-56 rounded-xl overflow-hidden border border-white/20 mx-auto">
                 <Image
-                  src={encodeURI("/images/foto integrantes.png")}
+                  src="/../public/images/Foto Integrantes.png"
                   alt="Foto dos integrantes"
                   width={960}
                   height={640}

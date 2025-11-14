a658b3110a6d6fc823b63e66f7fc73a74726ce35
Victor Lacomb de Melo <57971012+lacomb04@users.noreply.github.com>
2025-11-14 02:42:54 +0000

    Importando no footer maneira correta

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import FeedbackForm from "@/components/FeedbackForm";
+import FeedbackForm from "@/components/feedback-form";
 
 export default function Footer() {
   return (

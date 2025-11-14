994b8628be6dac024661ac66504c0d247ef1e464
Victor Lacomb de Melo <57971012+lacomb04@users.noreply.github.com>
2025-11-14 02:22:13 +0000

    Removendo efeito de digitação causando bug

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -1,30 +1,17 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
+import { useState } from "react";
 
 interface CodeBlockProps {
-  code: string
-  language?: string
+  code: string;
+  language?: string;
 }
 
-export default function CodeBlock({ code, language = "javascript" }: CodeBlockProps) {
-  const [displayedCode, setDisplayedCode] = useState("")
-  const [isComplete, setIsComplete] = useState(false)
-
-  useEffect(() => {
-    let index = 0
-    const interval = setInterval(() => {
-      if (index < code.length) {
-        setDisplayedCode((prev) => prev + code[index])
-        index++
-      } else {
-        setIsComplete(true)
-        clearInterval(interval)
-      }
-    }, 5)
-
-    return () => clearInterval(interval)
-  }, [code])
+export default function CodeBlock({
+  code,
+  language = "javascript",
+}: CodeBlockProps) {
+  // O código agora é exibido completo imediatamente
 
   return (
     <div className="bg-black/50 border border-cyan-500/30 rounded-lg overflow-hidden">
@@ -38,10 +25,9 @@ export default function CodeBlock({ code, language = "javascript" }: CodeBlockPr
       </div>
       <pre className="p-6 overflow-x-auto">
         <code className="font-mono text-sm text-gray-300 whitespace-pre-wrap break-words">
-          {displayedCode}
-          {!isComplete && <span className="animate-pulse text-cyan-400">▌</span>}
+          {code}
         </code>
       </pre>
     </div>
-  )
+  );
 }
